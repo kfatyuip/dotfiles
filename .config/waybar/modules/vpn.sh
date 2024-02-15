2045e67e784f7d2f5e429e5a42869e598b11cc30
Kieran Moy <kfatyuip@gmail.com>
2024-02-15 13:46:21 +0800

    update

--- .config/waybar/modules/vpn.sh
+++ .config/waybar/modules/vpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ip link show dev wg0 | grep -q 'wg0' \
+ip link show | grep -q 'wg0' \
 && echo '{"text":"Connected","class":"connected","percentage":100}' \
 || echo '{"text":"Disconnected","class":"disconnected","percentage":0}'
